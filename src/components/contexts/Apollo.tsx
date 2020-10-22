146d4f356d267d13e1729d41f1a8504cacf9af96
TwitoCode <toheebeji@gmail.com>
2020-10-22 19:57:31 -0400

    chaned to production mode

--- src/components/contexts/Apollo.tsx
+++ src/components/contexts/Apollo.tsx
@@ -1,11 +1,11 @@
 import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client';
 import React, { FC } from 'react';
 
-const devURI = 'http://localhost:5000';
-// const prodURI = 'https://twito-post-it-server.herokuapp.com/';
+// const devURI = 'http://localhost:5000';
+const prodURI = 'https://twito-post-it-server.herokuapp.com/';
 
 const client = new ApolloClient({
-	link: new HttpLink({ uri: devURI }),
+	link: new HttpLink({ uri: prodURI }),
 	cache: new InMemoryCache(),
 });
 
